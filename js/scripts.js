5af42ca3f967c4439592c6ae9ad3a61a8f8cdee7
Mr-Macko <max.allesch@gmx.de>
2021-10-11 17:53:48 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+alert("Hello World")
+
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
