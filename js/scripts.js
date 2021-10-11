995fff89ac55277d3adecc7645e12e6cf8b9c677
Mr-Macko <max.allesch@gmx.de>
2021-10-11 18:03:45 +0200

    deleted variables

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,3 @@ alert("Hello World")
 let favoriteFood = "Pizza";
 document.write(favoriteFood);
 
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
