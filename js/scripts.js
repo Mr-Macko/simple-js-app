1139cb2cbb205dfe8ed0a914051684c48c65c3c6
Mr-Macko <max.allesch@gmx.de>
2021-10-14 22:07:06 +0200

    added pokemonList
    Array with several pokemons

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'bulbasaur', height= '7', type: ['grass', 'poison']}
+    {name: 'charmander', height= '6', type: ['fire']}
+    {name: 'squirtle', height= '5', type: ['water']}
+]
