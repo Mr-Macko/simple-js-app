ed1b7004285e631163752792f0934a0e106ca0e6
Mr-Macko <max.allesch@gmx.de>
2021-10-20 22:48:37 +0200

    updated height values in array

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,6 @@ let pokemonList = [
     {name: 'charmander', height: '6', type: ['fire']},
     {name: 'squirtle', height: '5', type: ['water']},
 ]
+    {name: 'bulbasaur', height: '0.7', type: ['grass', 'poison']},
+    {name: 'charmander', height: '0.6', type: ['fire']},
+    {name: 'squirtle', height: '0.5', type: ['water']},
