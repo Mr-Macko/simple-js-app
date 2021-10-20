46b869e45dfd96afa14ee3b2f5dccc5354800c65
Mr-Macko <max.allesch@gmx.de>
2021-10-20 22:49:43 +0200

    added document.write loops

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,8 @@ let pokemonList = [
     {name: 'bulbasaur', height: '0.7', type: ['grass', 'poison']},
     {name: 'charmander', height: '0.6', type: ['fire']},
     {name: 'squirtle', height: '0.5', type: ['water']},
+// prints list (name and height) of all containing pokemon in dataset
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 0.6) document.write( '<p>' + pokemonList[i].name + ' (heigt: ' + pokemonList[i].height + ')' + ' - Wow! That is tall.' + '</p>');
+  else document.write('<p>' + pokemonList[i].name + ' (heigt: ' + pokemonList[i].height + ')' + '</p>');
+};
