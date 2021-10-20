545b2943de226c115bd6aa121fd4400453710f7e
Mr-Macko <max.allesch@gmx.de>
2021-10-20 22:50:03 +0200

    comment and spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// array dataset with pokemon
 let pokemonList = [
     {name: 'bulbasaur', height: '7', type: ['grass', 'poison']},
     {name: 'charmander', height: '6', type: ['fire']},
@@ -6,6 +7,8 @@ let pokemonList = [
     {name: 'bulbasaur', height: '0.7', type: ['grass', 'poison']},
     {name: 'charmander', height: '0.6', type: ['fire']},
     {name: 'squirtle', height: '0.5', type: ['water']},
+];
+
 // prints list (name and height) of all containing pokemon in dataset
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 0.6) document.write( '<p>' + pokemonList[i].name + ' (heigt: ' + pokemonList[i].height + ')' + ' - Wow! That is tall.' + '</p>');
