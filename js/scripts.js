265f49cf7f5169d03d8efbcc4b7f7c17679a2e05
Mr-Macko <max.allesch@gmx.de>
2021-10-29 16:21:05 +0200

    overwrite old array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 // array dataset with pokemon
 let pokemonList = [
-    {name: 'bulbasaur', height: '0.7', type: ['grass', 'poison']},
-    {name: 'charmander', height: '0.6', type: ['fire']},
-    {name: 'squirtle', height: '0.5', type: ['water']},
   {
     name: 'bulbasaur',
     height: '0.7',
