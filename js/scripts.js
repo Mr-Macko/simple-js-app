9ba684322ded3d51f45183e3a393e113f6753a2d
Mr-Macko <max.allesch@gmx.de>
2021-10-29 16:21:29 +0200

    changed for loop to forEach method

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,9 @@ let pokemonList = [
   },
 ];
 
-// prints list (name and height) of all containing pokemon in dataset
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 0.6) document.write('<p id=pokedex-style>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow! That is tall.' + '</p>');
-  else document.write('<p id=pokedex-style>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
-};
+// UPDATED to forEach: prints list (name and height) of all containing pokemon in dataset. 
+pokemonList.forEach(function(pokemon){
+document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')')
+if (pokemon.height > 0.6) document.write(' - Wow! that is tall')
+else document.write('</p>');
+});
