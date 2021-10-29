43b8a12a71f8a57addac36370fa679445d1e78d0
Mr-Macko <max.allesch@gmx.de>
2021-10-29 16:20:27 +0200

    added iterations to objects in array

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,21 @@ let pokemonList = [
     {name: 'bulbasaur', height: '0.7', type: ['grass', 'poison']},
     {name: 'charmander', height: '0.6', type: ['fire']},
     {name: 'squirtle', height: '0.5', type: ['water']},
+  {
+    name: 'bulbasaur',
+    height: '0.7',
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'charmander',
+    height: '0.6',
+    type: ['fire']
+  },
+  {
+    name: 'squirtle',
+    height: '0.5',
+    type: ['water']
+  },
 ];
 
 // prints list (name and height) of all containing pokemon in dataset
